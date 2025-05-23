510151632e33819752889a920e823a090caa2889
kallappa2005 <kallappakabbur874@gmail.com>
2025-05-23 17:18:22 +0530

    Initial Commit Of Ecommerce Web App

---
+++ frontend/src/pages/Verify.jsx
@@ -0,0 +1,46 @@
+import React, { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useSearchParams } from "react-router-dom";
+
+const verify = () => {
+  const { navigate, token, setCartItems, backendUrl } = useContext(ShopContext);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const success = searchParams.get("success");
+  const orderId = searchParams.get("orderId");
+
+  const verifyPayment = async () => {
+    try {
+      if (!token) {
+        null;
+      }
+      const response = await axios.post(
+        backendUrl + "/api/order/verifyStripe",
+        { success, orderId },
+        { headers: { token } }
+      );
+
+      if (response.data.success) {
+        setCartItems({});
+        navigate("/orders");
+      } else {
+        navigate("/cart");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    verifyPayment();
+  }, [token]);
+
+  return <div></div>;
+};
+
+export default verify;
